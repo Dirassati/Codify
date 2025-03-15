25601c35d4096b70f469d2386a214ce1147799c7
29sara29 <sn.khiter@esi-sba.dz>
2025-03-15 21:14:57 +0000

    auth

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const accountRoutes = require('./src/routes/accountRoutes');
+const authRoutes = require("./src/routes/authRoutes");
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api', accountRoutes);
+app.use("/api", authRoutes);
 
 // Test endpoint
 app.get('/', (req, res) => {
