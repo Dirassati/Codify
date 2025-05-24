491a287426639b95b05921974f0d700705d41f8e
jalil <abdeljalil.sen@gmail.com>
2025-05-24 20:16:47 +0200

    /

--- frontend/src/functions/Logout.jsx
+++ frontend/src/functions/Logout.jsx
@@ -1,10 +1,10 @@
 // src/functions/Logout.js or Logout.jsx (doesn't need to be JSX)
 export const handleLogout = async (setUser, navigate) => {
   try {
-    // await fetch('http://localhost:5000/logout', {
-    //   method: 'POST',
-    //   credentials: 'include',
-    // });
+    await fetch('http://localhost:5000/logout', {
+      method: 'POST',
+      credentials: 'include',
+    });
 
     setUser(null);
     navigate('/login');
