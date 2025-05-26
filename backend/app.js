6567f65ebb1c140815f7de97a12a5f6cd85f17f4
29sara29 <sn.khiter@esi-sba.dz>
2025-05-26 19:29:10 +0000

    //

--- backend/app.js
+++ backend/app.js
@@ -17,13 +17,10 @@ const timetableRoutes = require('./src/routes/timetableRoutes');
 const reinscriptionRoutes = require("./src/routes/re-inscriptionRoutes");
 const notesRoutes = require('./src/routes/notesRoutes');
 const inactivateOldReinscriptions = require("./src/utils/inactivateOldReinscriptions");
-<<<<<<< HEAD
+
 const errorMiddleware = require("./src/middleware/errorMiddleware");
 const { testConnection } = require("./testDbConnection");
 const notesService = require("./src/services/notesService");
-=======
-const notesService = require('./src/services/notesService');
->>>>>>> 8275847e6153040f4d1e07659e5ba6efac363622
 
 const authenRoutes = require('./src/routes/authenRoutes');
 const chatRoutes = require('./src/routes/chatRoutes');
@@ -39,7 +36,6 @@ const server = http.createServer(app);
 
 const PORT = process.env.PORT || 5000;
  
-<<<<<<< HEAD
 
 app.use(
   cors({
@@ -48,15 +44,6 @@ app.use(
   })
 );
 
-=======
-
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
->>>>>>> 8275847e6153040f4d1e07659e5ba6efac363622
 
 app.use(express.json());
 
